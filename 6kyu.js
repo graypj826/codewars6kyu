0be3fc89fde4ebedac413a833ba899254c1f15fe
Patrick <graypj826@gmail.com>
2018-10-09 08:19:36 -0600

    added test in the event array is empty

--- 6kyu.js
+++ 6kyu.js
@@ -3,20 +3,25 @@
 function list(names){
     let namesToBeString = []
     let namesAsString = ""
-     for (let i = 0; i < names.length; i++){
-       if (names[i].name === names[names.length-1].name ){
-         namesAsString = namesAsString + " & " + names[i].name
-         console.log(namesAsString)
+    if (names.length < 1){
+         console.log(names)
          return namesAsString
-      } else {
-        namesToBeString.push(names[i].name)
-        namesAsString = namesToBeString.join(", ")
-        console.log(namesAsString)
-      }
-       
+    } else if (names.length === 1){
+         return names[0].name
+    } else {
+       for (let i = 0; i < names.length; i++){ 
+         if (names[i].name === names[names.length-1].name ){
+           namesAsString = namesAsString + " & " + names[i].name
+           return namesAsString
+          } else {
+          namesToBeString.push(names[i].name)
+          namesAsString = namesToBeString.join(", ")
+          }
+       }  
      }
      
   }
+  
 
-  list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}])
+list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}])
   
